86b955547b02ae4d7bc4385eaa78c74ca645caba
TheCtac <thectac0211@gmail.com>
2019-01-25 15:48:05 +0200

    ref_node

--- test.js
+++ test.js
@@ -1,12 +1,4 @@
-/* NodeJS uses CommonJS, thatswhy necessary to use babel lib
-    run  npm init -y to create package.JSON
-    run npm install babel-cli babel-core babel-preset-es2015 --save-dev
-        to install tree babel packegs
-     run npm install to install all needed modules wroted in packege.json
-     set directory srtucture
-    add to packege.json scripts list value
-        "build" : "babel in_ES6 -d out_CommonJS --presets es2015"
-
+/*
 ctrl + c - stop program
 */
 
@@ -15,21 +7,35 @@ var func_ = require('./myFunctions');
 var events = require('events')
 var url = require('url');
 var fs = require('fs')
+var express = require('express');
 
+var app = express();
 var myEmit = new events.EventEmitter();
+app.use(express.static(__dirname + '/public'));
+/*
 myEmit.on('someEvent', function(text) {
     //console.log(text);
 });
-
-// імітуєм створення події
+імітуєм створення події
 myEmit.emit('someEvent', func_.getToday());
-
+*/
 var url_ = "https://ru.aliexpress.com/item/KINGSEVEN-2018-HD/32850720771.html?spm=firedeals.home.products-list-accessories.4.32ee2817SLm9QI&pvid=ab942bf0-807b-4ac5-93cb-ce7c281a7e4e&gps-id=5310016&scm=1007.14831.114205.0&scm-url=1007.14831.114205.0&scm_id=1007.14831.114205.0"
 var q = url.parse(url_, true).query;
 
-//var html_ = fs.readFileSync('index.html');
+// create and routing using express methods
+app.get('/', function(req, res){
+    //fs.createReadStream(__dirname + '/html/index.html', 'utf8').pipe(res);
+    res.sendFile(__dirname + '/html/index.html', 'utf8');
+});
+app.get('/tanks', function(req, res){
+    res.sendFile(__dirname + '/html/index.html', 'utf8');
+});
 
-var server = http.createServer(function (req, res) {
+app.listen(8000);
+
+//var html_ = fs.readFileSync('index.html');
+// create and routing using nodejs methods
+/*var server = http.createServer(function (req, res) {
     console.log("current url : " + req.url);
     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8', 'title': 'Test'});
     if (req.url === '/' || req.url === '/tanks') {
@@ -41,3 +47,4 @@ var server = http.createServer(function (req, res) {
     }
 });
 server.listen(8000, '127.0.0.1');
+*/
